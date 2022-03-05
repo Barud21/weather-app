ac86a5b4d505c44bef673d84dfc83c40db17d95e
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2022-03-05 14:56:33 +0100

    Form with toggle switch added

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./Form.css";
+import Switch from "react-switch";
 
 const defaultFormData = {
   latitude: "",
@@ -10,6 +11,38 @@ const defaultFormData = {
 export default function Form() {
   const [formData, setFormData] = useState(defaultFormData);
   const { latitude, longitude, alternativeSource } = formData;
+  const [checked, setChecked] = useState(alternativeSource);
 
-  return <></>;
+  return (
+    <>
+      <h2>Provide latitude and longitude</h2>
+      <form>
+        <label htmlFor="latitude">LAT:</label>
+        <input
+          type="number"
+          id="latitude"
+          value={latitude}
+          min={-90}
+          max={90}
+        />
+        <br />
+        <label htmlFor="longitude">LON:</label>
+        <input
+          type="number"
+          id="longitude"
+          value={longitude}
+          min={-180}
+          max={180}
+        />
+        <br />
+        <Switch
+          onChange={handleChange}
+          checked={checked}
+          uncheckedIcon={false}
+          checkedIcon={false}
+        />
+        <button type="submit">Display weather</button>
+      </form>
+    </>
+  );
 }
