b506fee05830d7e980263d81a9a678aa46c1a453
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2022-03-05 14:35:04 +0100

    defaultFormData declared

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
 import "./Form.css";
 
+const defaultFormData = {
+  latitude: "",
+  longitude: "",
+  alternativeSource: false,
+};
+
 export default function Form() {
+  const [formData, setFormData] = useState(defaultFormData);
+  const { latitude, longitude, alternativeSource } = formData;
+
   return <></>;
 }
