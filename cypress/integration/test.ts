881a441f211bc8de2de47512e51dd2dd38a02356
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2022-03-07 10:43:44 +0100

    Test checks if does not get response when latitude is missing

--- cypress/integration/test.ts
+++ cypress/integration/test.ts
@@ -23,6 +23,17 @@ describe("test", () => {
     // then
     resultAfterSubmitting();
   });
+
+  it("checks if does not get response when latitude is missing", () => {
+    // given
+    websiteIsOpened();
+
+    // when
+    inputData(" ", "21");
+
+    // then
+    resultWhenDataIsMissing();
+  });
 });
 
 function websiteIsOpened() {
@@ -48,4 +59,10 @@ function resultAfterSubmitting() {
   cy.get(".temp").should("not.equal", "");
   cy.get(".pressure").should("not.equal", "");
   cy.get(".humidity").should("not.equal", "");
+}
+
+function resultWhenDataIsMissing() {
+  cy.get(".temp").should("be.empty");
+  cy.get(".pressure").should("be.empty");
+  cy.get(".humidity").should("be.empty");
 }
