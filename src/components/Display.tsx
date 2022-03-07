c8cb14d59afa7af66f96c517b094df1de1bed855
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2022-03-07 19:47:30 +0100

    Element is hidden if there is no data to display, ids added to elements (for easier testing)

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -6,25 +6,33 @@ const Display: React.FC<{
   pressure: number | undefined;
   humidity: number | undefined;
 }> = ({ temperature, pressure, humidity }) => {
+  if (temperature === undefined) {
+    document.getElementById("display-container")?.classList.add("transparent");
+  } else {
+    document
+      .getElementById("display-container")
+      ?.classList.remove("transparent");
+  }
+
   return (
     <>
-      <div className="container">
+      <div className="container-display transparent" id="display-container">
         <div className="container-temp">
           <div className="temp">
-            <p>{temperature}</p>°C
+            <p id="temp">{temperature}</p>°C
           </div>
           <div>Temperature</div>
         </div>
         <div className="container-pres-hum">
           <div className="container-temp">
             <div className="pressure">
-              <p>{pressure}</p>hPa
+              <p id="pressure">{pressure}</p>hPa
             </div>
             <div>Pressure</div>
           </div>
           <div className="container-temp">
             <div className="humidity">
-              <p>{humidity}</p>%
+              <p id="humidity">{humidity}</p>%
             </div>
             <div>Humidity</div>
           </div>
